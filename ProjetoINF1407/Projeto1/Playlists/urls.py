a694df266619ed2a89e2e765f782d78842a2f271
Erica <ericaregnier@tecgraf.puc-rio.br>
2026-04-18 17:07:06 -0300

    trocar senha funcionando

--- ProjetoINF1407/Projeto1/Playlists/urls.py
+++ ProjetoINF1407/Projeto1/Playlists/urls.py
@@ -1,10 +1,8 @@
-from django.urls import path
 from Playlists import views
-
-# urlpatterns = [
-#     path('', views.home),
-# ] 
-
+from django.contrib import admin
+from django.urls import path, include
+from django.urls import reverse_lazy
+from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView
 
 urlpatterns = [
     path('', views.home, name='homepage'),
@@ -12,4 +10,20 @@ urlpatterns = [
     path('adicionar/', views.adicionar_musica, name='adicionar_musica'),
     path('editar/<int:id>/', views.editar_musica, name='editar_musica'),
     path('remover/<int:id>/', views.remover_musica, name='remover_musica'),
+    path(
+    'seguranca/password_change/',
+    PasswordChangeView.as_view(
+        template_name='seguranca/password_change_form.html',
+        success_url=reverse_lazy('sec-password_change_done'),
+    ),
+    name='sec-password_change'
+),
+
+path(
+    'seguranca/password_change_done/',
+    PasswordChangeDoneView.as_view(
+        template_name='seguranca/password_change_done.html',
+    ),
+    name='sec-password_change_done'
+),
 ]
